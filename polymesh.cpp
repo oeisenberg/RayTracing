6e9a9a113a7900b3341f78a11f768f156b9e9917
oeisenberg <oliver.eisenberg88@gmail.com>
2019-10-09 09:52:30 +0100

    Init of CW
    CW has achieved 100% in Lab 1 and Lab 2 so far. 
    Refactoring still needs to be completed in Lab 2 before submission.

---
+++ polymesh.cpp
@@ -0,0 +1,100 @@
+/***************************************************************************
+ *
+ * krt - Kens Raytracer - Coursework Edition. (C) Copyright 1997-2019.
+ *
+ * Do what you like with this code as long as you retain this comment.
+ */
+
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <algorithm>
+
+#include "polymesh.h"
+#include <typeinfo>
+
+using namespace std;
+
+PolyMesh::PolyMesh(char *file)
+{
+	Transform *transform = new Transform();
+
+	this->do_construct(file, transform);
+}
+
+PolyMesh::PolyMesh(char *file, Transform *transform)
+{
+	this->do_construct(file, transform);
+}
+
+void PolyMesh::do_construct(char *file, Transform *transform)
+{
+	std::ifstream newFile(file);
+	std::string line;
+
+	getline(newFile, line);
+	getline(newFile, line);
+	string newline = line.c_str();
+	
+	std::vector<std::string> tokens = PolyMesh::do_split(newline);
+	string noV = tokens.back();
+	PolyMesh::vertex_count = stoi(noV);
+
+	getline(newFile, line);
+	newline = line.c_str();
+
+	tokens = PolyMesh::do_split(newline);
+	string noF = tokens.back();
+	PolyMesh::triangle_count = stoi(noF);
+
+	Vertex *vertex = new Vertex[PolyMesh::vertex_count];
+	TriangleIndex *triangle = new TriangleIndex[PolyMesh::triangle_count];
+
+	for (int lineNo = 3; getline(newFile, line) && lineNo <= PolyMesh::vertex_count + PolyMesh::triangle_count + 3; lineNo++)
+	{
+		string newline = line.c_str();
+
+		std::vector<std::string> tokens = PolyMesh::do_split(newline);
+
+		if (tokens.size() == 3) // Vertex
+		{
+			float x = atof(tokens.at(0).c_str());
+			float y = atof(tokens.at(1).c_str());
+			float z = atof(tokens.at(2).c_str());
+
+			vertex[lineNo - 3] = Vertex(x, y, z);
+
+			transform->apply(vertex[lineNo - 3]);
+		}
+		else if (tokens.size() == 4) { //Face
+
+			for (int iToken = 1; iToken <= 3; iToken++) {
+				triangle[lineNo - PolyMesh::vertex_count - 3][iToken - 1] = atoi(tokens.at(iToken).c_str()) - 1;
+			}
+		}
+		else if (tokens.size() != 1) {
+			cerr << "Error, " << tokens.size() << ": ";
+		}
+	}
+
+	PolyMesh::vertex = vertex;
+	PolyMesh::triangle = triangle;
+
+	newFile.close();
+}
+
+std::vector<std::string> PolyMesh::do_split(std::string inputString)
+{
+	std::string segments;					 // Have a buffer string
+	std::stringstream ss(inputString);       // Insert the string into a stream
+	std::vector<std::string> output;
+
+	while (ss >> segments)
+	{
+		output.push_back(segments);
+	}
+
+	return output;
+}
---
+++ polymesh.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *
+ * krt - Kens Raytracer - Coursework Edition. (C) Copyright 1997-2019.
+ *
+ * Do what you like with this code as long as you retain this comment.
+ */
+
+#pragma once
+
+#include "vertex.h"
+#include "transform.h"
+#include <vector>
+#include <string>
+
+typedef int TriangleIndex[3];
+
+class PolyMesh {
+public:
+	int vertex_count;
+	int triangle_count;
+        Vertex *vertex;
+	TriangleIndex *triangle;
+
+	void do_construct(char *file, Transform *transform);
+	std::vector<std::string> do_split(std::string inputString);
+
+	PolyMesh(char *file);
+	PolyMesh(char *file, Transform *transform);
+};
