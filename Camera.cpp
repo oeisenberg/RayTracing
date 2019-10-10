96220ff3c2c7bedbd8f7deb637ccaf5441c2334a
Oliver Eisenberg <ohe21@bath.ac.uk>
2019-10-10 14:47:15 +0100

    Adding new camera files to be initialised in main lab 3

---
+++ Camera.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+
+#include "Camera.h"
+
+Camera::Camera()
+{
+	
+}
---
+++ Camera.h
@@ -0,0 +1,17 @@
+/***************************************************************************
+ *
+ * krt - Kens Raytracer - Coursework Edition. (C) Copyright 1997-2019.
+ *
+ * Do what you like with this code as long as you retain this comment.
+ */
+
+#ifndef _CAMERA_H_
+#define _CAMERA_H_
+
+class Camera {
+
+public:
+
+};
+
+#endif
